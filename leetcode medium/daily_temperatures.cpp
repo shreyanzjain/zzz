22d1ae958c2e3c06b12d033730223b943261668e
Shreyans Jain <51560928+shreyanzjain@users.noreply.github.com>
2023-06-18 21:07:29 +0530

    update: daily temperatures

--- leetcode medium/daily_temperatures.cpp
+++ leetcode medium/daily_temperatures.cpp
@@ -1,3 +1,10 @@
+/*Stack
+Keep storing index's onto the stack, 
+as long as a higher temperature is not found,
+once found - keep popping index's that have lower temps
+than the current index, store the diff as the ans at that index
+then finally push the current value and repeat the same process
+till you reach the end of the array*/
 class Solution {
 public:
     vector<int> dailyTemperatures(vector<int>& temperatures) {
