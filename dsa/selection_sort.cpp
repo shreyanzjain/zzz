f9e0359e0ea8dc01c2e5201598a2da4341d51a8e
shreyanzjain <shreyanz32@gmail.com>
2023-07-22 22:17:04 +0530

    update: selection sort

--- dsa/selection_sort.cpp
+++ dsa/selection_sort.cpp
@@ -7,49 +7,38 @@ Find minimum element in the array at each pass and swap it with the current elem
 
 using namespace std;
 
-int main()
-{
-    vector<int> nums;
-    int size;
-    int target;
-    int res = 0;
+void swap_in_place(int &a, int &b){
+    int temp = a;
+    a = b;
+    b = temp;
+}
 
-    // taking input
-    cout << "Enter the number of elements: ";
-    cin >> size;
-
-    cout << "Enter the elements: ";
-    for (int i = 0; i < size; i++)
-    {
-        int val;
+int main(){
+    int n;
+    cin >> n;
+    int val;
+    
+    vector<int> arr;
+    for(int i = 0; i < n; i++){
         cin >> val;
-        nums.push_back(val);
+        arr.push_back(val);
     }
-    int min;
-    int index;
 
-    for (int i = 0; i < nums.size(); i++)
-    {
-        min = nums[i];
-        index = i;
-        for (int j = i + 1; j < nums.size(); j++)
-        {
-            if (nums[j] < min)
-            {
-                min = nums[j];
-                index = j;
+    for(int i = 0; i < n; i++){
+        // assuming that the current element is smallest
+        int smallest_idx = i;
+
+        for(int j = i + 1; j < n; j++){
+            // if there is a smaller element, then update the smallest_idx
+            if(arr[j] < arr[smallest_idx]){
+                smallest_idx = j;
             }
         }
-        int temp = nums[i];
-        nums[i] = min;
-        nums[index] = temp;
+        swap_in_place(arr[i], arr[smallest_idx]);
     }
 
-    cout << "Sorted elements: ";
-    for (int i = 0; i < nums.size(); i++)
-    {
-        cout << nums[i] << " ";
-    }
-    cout << endl;
+    for(int i = 0; i < n; i++){
+        cout << arr[i] << " ";
+    }   cout << endl;
     return 0;
 }
