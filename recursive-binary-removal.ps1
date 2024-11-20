4957ceda335aae28cd070b18c7d5fd1e49409c70
shreyanzjain <shreyanz32@gmail.com>
2024-11-20 19:13:07 +0530

    WARNING

--- recursive-binary-removal.ps1
+++ recursive-binary-removal.ps1
@@ -1,5 +1,8 @@
 # script to recursively remove all the .exe 
 # files generated when compiling and running cpp files
+
+# DO NOT RUN THIS SCRIPT OUTSIDE OF THIS FOLDER
+# IT WILL DELETE ALL EXE FILES IN THE FOLDER YOU MOVE IT INTO
 $listItems = Get-ChildItem -Recurse -Include *.exe
 
 foreach ($item in $listItems) {
