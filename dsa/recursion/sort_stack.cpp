15bbe868a0eee74777d770bbc5573ecee8d4c89c
shreyanzjain <shreyanz32@gmail.com>
2025-10-23 19:44:11 +0530

    sort a stack recursively

---
+++ dsa/recursion/sort_stack.cpp
@@ -0,0 +1,58 @@
+// sorting a stack
+
+#include <iostream>
+#include <stack>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+    private:
+    public:
+        stack<int> recursive_sort(stack<int>& nums) {
+            if(nums.size() == 1) {
+                return nums;
+            }
+
+            int temp = nums.top();
+            nums.pop();
+
+            recursive_sort(nums);
+            recursive_insert(nums, temp);
+            
+            return nums;
+        }
+
+        stack<int> recursive_insert(stack<int>& nums, int value) {
+            if(nums.empty() || nums.top() >= value) {
+                nums.push(value);
+                return nums;
+            }
+
+            int temp = nums.top();
+            nums.pop();
+            recursive_insert(nums, value);
+            nums.push(temp);
+            return nums;
+        }
+};
+
+int main() {
+Solution sol;
+    vector<int> vect = {3, 2, 6, 1, 0, -11, 7, -12};
+    stack<int> nums;
+
+    for(int ele : vect) {
+        nums.push(ele);
+    }
+    
+    sol.recursive_sort(nums);
+
+    cout<<"After sort: ";
+    for(int i = 0; i < vect.size(); i++) {
+        cout<<nums.top()<<"\t";
+        nums.pop();
+    } cout<<endl;
+
+    return 0;
+}
